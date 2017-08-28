06db8406c85e2df7a90cec28c0a5e39e983f3629
Ulysse Carion <ulysse@ulysse.io>
2017-08-27 23:54:19 -0700

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,37 @@
+//! This crate is a tiny utility library for finding the intersection of two 2D line segments.
+//!
+//! To use this library, construct a `LineSegment<T: num_traits::Float>` and `relate` it with
+//! another `LineSegment`. This will return a `LineRelation<T>`, which covers all the possible
+//! relations between those two lines.
+//!
+//! `LineRelation<T>` provides `unique_intersection`, if all you care about is finding a unique
+//! intersection, and (for example) you don't care to distinguish cases where there are zero or an
+//! infinite number of intersections.
+//!
+//! For your convenience, this library provides a top-level `relate` function for comparing two
+//! values that can be converted into `LineSegment`s. Such a conversion has been provided for `[[T;
+//! 2]; 2]`.
+//!
+//! ```
+//! use line_segment_intersection::relate;
+//!
+//! let line1 = [[0.0, 0.0], [1.0, 1.0]];
+//! let line2 = [[1.0, 0.0], [0.0, 1.0]];
+//!
+//! assert_eq!(Some([0.5, 0.5]), relate(&line1, &line2).unique_intersection());
+//! ```
+
 extern crate num_traits;
 
 use num_traits::Float;
 
+/// A line segment in two-dimensional space, defined from its two endpoints.
+///
+/// For the purposes of this library, the order of `from` and `to` does not matter; swapping the
+/// values of `from` and `to` will never affect the result of calling `LineSegment::relate`.
 pub struct LineSegment<T> {
-    from: [T; 2],
-    to: [T; 2],
+    pub from: [T; 2],
+    pub to: [T; 2],
 }
 
 /// The relationship between two line segments.
@@ -33,7 +60,18 @@ pub enum LineRelation<T> {
     ParallelDisjoint,
 }
 
+impl<T> LineRelation<T> {
+    /// Get the point of intersection if this is a `DivergentIntersecting`, else return `None`.
+    pub fn unique_intersection(self) -> Option<[T; 2]> {
+        match self {
+            LineRelation::DivergentIntersecting(p) => Some(p),
+            _ => None,
+        }
+    }
+}
+
 impl<T: Float> LineSegment<T> {
+    /// Get the relationship between this line segment and another.
     pub fn relate(&self, other: &LineSegment<T>) -> LineRelation<T> {
         let sub = |a: &[T; 2], b: &[T; 2]| -> [T; 2] {
             [a[0] - b[0], a[1] - b[1]]
@@ -100,11 +138,14 @@ impl<T: Float> LineSegment<T> {
    }
 }
 
+/// Convert `a` and `b` into `LineSegment`s, and `relate` them.
 pub fn relate<T: Float, S: Into<LineSegment<T>>>(a: S, b: S) -> LineRelation<T> {
     a.into().relate(&b.into())
 }
 
 impl<'a, T: Copy> From<&'a [[T; 2]; 2]> for LineSegment<T> {
+    /// Construct a `LineSegment` from a pair of points; the first `[T; 2]` will be `from`, and the
+    /// second will be `to`.
     fn from(point_pair: &'a [[T; 2]; 2]) -> LineSegment<T> {
         LineSegment {
             from: point_pair[0],
@@ -122,6 +163,7 @@ mod tests {
         let a = [[1.0, 0.0], [1.0, 1.0]];
         let b = [[0.0, 0.0], [2.0, 0.5]];
         test_point_relations(&a, &b, LineRelation::DivergentIntersecting([1.0, 0.25]));
+        assert_eq!(Some([1.0, 0.25]), relate(&a, &b).unique_intersection());
     }
 
     #[test]
@@ -129,6 +171,7 @@ mod tests {
         let a = [[0.0, 0.0], [1.0, 1.0]];
         let b = [[3.0, 0.0], [0.0, 3.0]];
         test_point_relations(&a, &b, LineRelation::DivergentDisjoint);
+        assert_eq!(None, relate(&a, &b).unique_intersection());
     }
 
     #[test]
@@ -136,6 +179,7 @@ mod tests {
         let a = [[0.0, 0.0], [1.0, 1.0]];
         let b = [[0.0, 1.0], [1.0, 2.0]];
         test_point_relations(&a, &b, LineRelation::ParallelDisjoint);
+        assert_eq!(None, relate(&a, &b).unique_intersection());
     }
 
     #[test]
@@ -143,6 +187,7 @@ mod tests {
         let a = [[0.0, 0.0], [0.0, 2.0]];
         let b = [[0.0, 1.0], [0.0, 1.5]];
         test_point_relations(&a, &b, LineRelation::CollinearOverlapping);
+        assert_eq!(None, relate(&a, &b).unique_intersection());
     }
 
     #[test]
@@ -150,6 +195,7 @@ mod tests {
         let a = [[0.0, 0.0], [0.0, 1.0]];
         let b = [[0.0, 2.0], [0.0, 3.0]];
         test_point_relations(&a, &b, LineRelation::CollinearDisjoint);
+        assert_eq!(None, relate(&a, &b).unique_intersection());
     }
 
     fn test_point_relations(a: &[[f64; 2]; 2], b: &[[f64; 2]; 2], relation: LineRelation<f64>) {
